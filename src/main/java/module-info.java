0c4f7ef44c963a8bbd1d7f12f57d0cd2ff3bc2d8
Manuel García <manuel.garcia.rey.48@gmail.com>
2026-05-22 19:04:29 +0200

    fix: corregido error de acceso modular abriendo y exportando el paquete controller a javafx.fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,11 @@ module com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx {
     requires javafx.fxml;
     requires java.sql;
 
-
-    opens com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx to javafx.fxml;
     exports com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx;
+    opens com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx to javafx.fxml;
+    exports com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx.controller;
+    opens com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx.controller to javafx.fxml;
+    exports com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx.util;
+    opens com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx.util to javafx.fxml;
+    exports com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx.model;
 }
