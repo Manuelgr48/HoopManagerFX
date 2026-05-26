6bc58eb2d1f34284ac9cf329eb86bf86d125f85f
Manuel García <manuel.garcia.rey.48@gmail.com>
2026-05-26 15:54:58 +0200

    feat: configurar ventana maximizada y añadir favicon

--- src/main/java/com/liceolapaz/mgr/jugadores2ev/hoopmanagerfx/MainApp.java
+++ src/main/java/com/liceolapaz/mgr/jugadores2ev/hoopmanagerfx/MainApp.java
@@ -3,6 +3,7 @@ package com.liceolapaz.mgr.jugadores2ev.hoopmanagerfx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,15 @@ public class MainApp extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("HoopManagerFX - Acceso");
         stage.setScene(scene);
+        stage.setMaximized(true);
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("/images/favicon_basketball.png"));
+            stage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.out.println("No se pudo cargar el icono: " + e.getMessage());
+        }
+
+        stage.show();
         stage.show();
     }
 
